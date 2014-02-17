704c3574e0c66dca043fdef2ee1d2eb2f273b9b6
wakeupmrkim <alexkim27@gmail.com>
2014-02-17 16:09:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iloveit::Application.config.secret_token = '1a8c17bdc9415ddc5469737e190641e30e80d0f41858f8d108dc68ad4a66931765c5b92ea283f12d835392f4bac87807d9b00cab22021c8adc47a7125b2122e1'
